98da6f81b98bcaef42a8adbb70d8a6c40b25bec0
Teodorescu Mihail <mihail.teodorescu>
2020-04-10 17:10:28 +0300

    add remove command

--- BDInfo.Core/ps/create_machine.ps1
+++ BDInfo.Core/ps/create_machine.ps1
@@ -1,2 +1,5 @@
 ﻿az login
-az deployment group create --name ExampleDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters.json
+az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters.json
+
+
+#az group delete UbuntuRG
