644a7bf9b34650c79c938cd2c0afb837eca4a1e2
Teodorescu Mihail <mihail.teodorescu>
2020-04-13 17:09:58 +0300

    remove script since they are in separate repo

--- BDInfo.Core/ps/create_machine.ps1
+++
@@ -1,6 +0,0 @@
-﻿az login
-az deployment group create --name MyMachineDeployment --resource-group UbuntuRG --template-file template.json --parameters parameters.json
-
-
-#az group delete --name UbuntuRG
-#az group delete --name NetworkWatcherRG
