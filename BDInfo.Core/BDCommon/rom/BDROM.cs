eb2b766daff7df61e0fda66d0b73017e68f150ce
Mihail Teodorescu <mihail.teodorescu@cegeka.com>
2020-06-03 12:50:35 +0300

    fix if no xml valid

--- BDInfo.Core/BDCommon/rom/BDROM.cs
+++ BDInfo.Core/BDCommon/rom/BDROM.cs
@@ -410,17 +410,26 @@ namespace BDCommon
 
     private void ReadDiscTitle(StreamReader fileStream)
     {
-      XmlDocument xDoc = new XmlDocument();
-      xDoc.Load(fileStream);
-      var xNsMgr = new XmlNamespaceManager(xDoc.NameTable);
-      xNsMgr.AddNamespace("di", "urn:BDA:bdmv;discinfo");
-      var xNode = xDoc.DocumentElement?.SelectSingleNode("di:discinfo/di:title/di:name", xNsMgr);
-      DiscTitle = xNode?.InnerText;
+      try
+      {
+        XmlDocument xDoc = new XmlDocument();
+        xDoc.Load(fileStream);
+        var xNsMgr = new XmlNamespaceManager(xDoc.NameTable);
+        xNsMgr.AddNamespace("di", "urn:BDA:bdmv;discinfo");
+        var xNode = xDoc.DocumentElement?.SelectSingleNode("di:discinfo/di:title/di:name", xNsMgr);
+        DiscTitle = xNode?.InnerText;
 
-      if (!string.IsNullOrEmpty(DiscTitle) && DiscTitle.ToLowerInvariant() == "blu-ray")
-        DiscTitle = null;
-
-      fileStream.Close();
+        if (!string.IsNullOrEmpty(DiscTitle) && DiscTitle.ToLowerInvariant() == "blu-ray")
+          DiscTitle = null;
+      }
+      catch (Exception)
+      {
+        DiscTitle = VolumeLabel;
+      }
+      finally
+      {
+        fileStream.Close();
+      }
     }
 
     public void Scan()
