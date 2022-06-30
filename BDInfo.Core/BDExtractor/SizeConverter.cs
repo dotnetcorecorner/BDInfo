f85cff2017f069819e5b24b89fbbe364a8998846
Mihail Teodorescu <mihail.teodorescu@softelligence.net>
2022-06-30 15:23:18 +0300

    format space

--- BDInfo.Core/BDExtractor/SizeConverter.cs
+++ BDInfo.Core/BDExtractor/SizeConverter.cs
@@ -5,14 +5,14 @@ namespace BDExtractor
 {
 	internal static class SizeConverter
 	{
-    public static string SizeToText(double size)
-    {
-      var units = new string[] { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
-      const ushort divide = 1000;
+		public static string SizeToText(double size)
+		{
+			var units = new string[] { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+			const ushort divide = 1000;
 
-      int power = size > 0 ? (int)Math.Floor(Math.Log(size, divide)) : 0;
+			int power = size > 0 ? (int)Math.Floor(Math.Log(size, divide)) : 0;
 
-      return $"{Math.Round(size / Math.Pow(divide, power), 2).ToString(CultureInfo.InvariantCulture)} {units[power]}";
-    }
-  }
+			return $"{Math.Round(size / Math.Pow(divide, power), 2).ToString(CultureInfo.InvariantCulture)} {units[power]}";
+		}
+	}
 }
