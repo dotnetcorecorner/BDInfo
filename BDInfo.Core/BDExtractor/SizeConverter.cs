83abe990f5d2051cd0344164c7465b4144ccff26
Mihail Teodorescu <mihail.teodorescu@softelligence.net>
2022-06-30 13:23:55 +0300

    use constant

--- BDInfo.Core/BDExtractor/SizeConverter.cs
+++ BDInfo.Core/BDExtractor/SizeConverter.cs
@@ -8,9 +8,11 @@ namespace BDExtractor
     public static string SizeToText(double size)
     {
       var units = new string[] { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
-      int power = size > 0 ? (int)Math.Floor(Math.Log(size, 1000)) : 0;
+      const uint divide = 1000;
 
-      return $"{Math.Round(size / Math.Pow(1000, power), 2).ToString(CultureInfo.InvariantCulture)} {units[power]}";
+      int power = size > 0 ? (int)Math.Floor(Math.Log(size, divide)) : 0;
+
+      return $"{Math.Round(size / Math.Pow(divide, power), 2).ToString(CultureInfo.InvariantCulture)} {units[power]}";
     }
   }
 }
