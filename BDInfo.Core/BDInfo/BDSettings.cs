4dd30c3db24724af107783944e766532d4fb5c57
Sonic3R <teodorescumihail2008@gmail.com>
2025-01-10 17:00:04 +0200

    default stuff

--- BDInfo.Core/BDInfo/BDSettings.cs
+++ BDInfo.Core/BDInfo/BDSettings.cs
@@ -4,49 +4,49 @@ using System.IO;
 
 namespace BDInfo
 {
-	public sealed class BDSettings : BDInfoSettings
-	{
-		private readonly CmdOptions _opts;
+    public sealed class BDSettings : BDInfoSettings
+    {
+        private readonly CmdOptions _opts;
 
-		public BDSettings(CmdOptions opts)
-		{
-			_opts = opts;
-		}
+        public BDSettings(CmdOptions opts)
+        {
+            _opts = opts;
+        }
 
-		public override bool GenerateStreamDiagnostics => _opts?.GenerateStreamDiagnostics ?? true;
+        public override bool GenerateStreamDiagnostics => _opts?.GenerateStreamDiagnostics ?? true;
 
-		public override bool ExtendedStreamDiagnostics => _opts?.ExtendedStreamDiagnostics ?? false;
+        public override bool ExtendedStreamDiagnostics => _opts?.ExtendedStreamDiagnostics ?? false;
 
-		public override bool EnableSSIF => _opts?.EnableSSIF ?? true;
+        public override bool EnableSSIF => _opts?.EnableSSIF ?? true;
 
-		public override bool DisplayChapterCount => _opts?.DisplayChapterCount ?? false;
+        public override bool DisplayChapterCount => _opts?.DisplayChapterCount ?? false;
 
-		public override bool GenerateFrameDataFile => _opts?.GenerateFrameDataFile ?? false;
+        public override bool GenerateFrameDataFile => _opts?.GenerateFrameDataFile ?? false;
 
-		public override bool FilterLoopingPlaylists => _opts?.FilterLoopingPlaylists ?? true;
+        public override bool FilterLoopingPlaylists => _opts?.FilterLoopingPlaylists ?? true;
 
-		public override bool FilterShortPlaylists => _opts?.FilterShortPlaylists ?? true;
+        public override bool FilterShortPlaylists => _opts?.FilterShortPlaylists ?? true;
 
-		public override int FilterShortPlaylistsValue => _opts?.FilterShortPlaylistsValue ?? 20;
+        public override int FilterShortPlaylistsValue => _opts?.FilterShortPlaylistsValue ?? 20;
 
-		public override bool UseImagePrefix => _opts?.UseImagePrefix ?? false;
+        public override bool UseImagePrefix => _opts?.UseImagePrefix ?? false;
 
-		public override string UseImagePrefixValue => _opts?.UseImagePrefixValue ?? "video-";
+        public override string UseImagePrefixValue => _opts?.UseImagePrefixValue ?? "video-";
 
-		public override bool KeepStreamOrder => _opts?.KeepStreamOrder ?? true;
+        public override bool KeepStreamOrder => _opts?.KeepStreamOrder ?? false;
 
-		public override bool GenerateTextSummary => _opts?.GenerateTextSummary ?? true;
+        public override bool GenerateTextSummary => _opts?.GenerateTextSummary ?? true;
 
-		public override string ReportFileName => string.IsNullOrWhiteSpace(_opts?.ReportFileName) ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BDInfo_{0}.bdinfo") : _opts.ReportFileName;
+        public override string ReportFileName => string.IsNullOrWhiteSpace(_opts?.ReportFileName) ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BDInfo_{0}.bdinfo") : _opts.ReportFileName;
 
-		public override bool IncludeVersionAndNotes => _opts?.IncludeVersionAndNotes ?? false;
+        public override bool IncludeVersionAndNotes => _opts?.IncludeVersionAndNotes ?? false;
 
-		public override bool PrintOnlyForBigPlaylist => _opts?.PrintOnlyForBigPlaylist ?? true;
+        public override bool PrintOnlyForBigPlaylist => _opts?.PrintOnlyForBigPlaylist ?? true;
 
-		public override bool PrintReportToConsole => _opts?.PrintReportToConsole ?? false;
+        public override bool PrintReportToConsole => _opts?.PrintReportToConsole ?? false;
 
-		public override bool GroupByTime => _opts?.GroupByTime ?? false;
+        public override bool GroupByTime => _opts?.GroupByTime ?? false;
 
-		public bool IsExecutedAsScript => _opts?.IsScriptExecuted ?? false;
-	}
+        public bool IsExecutedAsScript => _opts?.IsScriptExecuted ?? false;
+    }
 }
